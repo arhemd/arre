6da6a0792e6ec3def16c1d5c13f99802a5b274bb
arhemd <arhemd@OS-kol.localdomain>
2019-12-30 16:13:15 +0330

    bash scripts update

--- iptables-setup.sh
+++ iptables-setup.sh
@@ -3,3 +3,5 @@ iptables -N PRX
 iptables -A PRX -j DROP
 iptables -A INPUT -p tcp --dport $port -j PRX
 iptables -A INPUT -p udp --dport $port -j PRX
+apt-get install iptables-persistent -y
+iptables-save > /etc/iptables/rules.v4
--- npm-setup.sh
+++ npm-setup.sh
@@ -1,11 +1,3 @@
-cd /root
-read -p 'Port to mangage: ' port
-iptables -N PRX
-iptables -A PRX -j DROP
-iptables -A INPUT -p tcp --dport $port -j PRX
-iptables -A INPUT -p udp --dport $port -j PRX
-apt-get install iptables-persistent -y
-iptables-save > /etc/iptables/rules.v4
 apt-get install curl -y
 curl -sL https://deb.nodesource.com/setup_13.x | sudo bash -
 apt-get install nodejs -y
